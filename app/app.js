509d222cb8bcf33c850ee2bd3638aa8fb6bbea86
James Agius <jamesagius@gmail.com>
2016-02-26 23:01:27 -0500

    Reverted back to criteria builder

--- app/app.js
+++ app/app.js
@@ -1,192 +1,149 @@
 'use strict';
 
 var app = angular.module('myApp', [
-    'ui.bootstrap','ui.grid','ui.grid.resizeColumns', 'ui.grid.selection', 'ui.grid.saveState', 'ui.grid.pagination'
+    'ui.bootstrap'
 ]);
 
-app.directive('pstd', function($compile) {
+app.controller('mainCtrl', function($scope) {
+    $scope.fields = [
+        { displayName: 'someStringField', modelName: 'someStringField', type: 'string'},
+        { displayName: 'someDateField', modelName: 'someDateField', type: 'date'},
+        { displayName: 'someBooleanField', modelName: 'someBooleanField', type: 'boolean'},
+        { displayName: 'someIntegerField', modelName: 'someIntegerField', type: 'integer'}
+    ];
+
+    $scope.anyOrAll = 'all';
+
+    $scope.placeholderArray = [{selected: $scope.fields[0]}];
+
+    $scope.plus = function() {
+        $scope.placeholderArray.push({value:''});
+    }
+
+    $scope.minus = function() {
+        $scope.placeholderArray.pop();
+    }
+
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    $scope.open = function($event) {
+        $scope.status.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy-MM-dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[1];
+
+    $scope.status = {
+        opened: false
+    };
+
+});
+
+app.directive('ngCriteria', function($compile) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
+        link: function(scope, element, attrs, tabsCtrl) {
 
-            function stringIt(someObject) {
-                var j = '';
-                angular.forEach(someObject, function(value, key) {
-                    if (jQuery.type(value) === "string") {
-                        j = j + key + ": '" + value + "',";
-                    } else if (jQuery.type(value) === "boolean") {
-                        if (value == false) {
-                            j = j + key + ": '" + 'PSFALSE' + "',";
-                        } else {
-                            j = j + key + ": '" + 'PSTRUE' + "',";
-                        }
-                    } else if(jQuery.type(value) === "number") {
-                        j = j + key + ": " + value + " ,";
-                    } else {
-                        j = j + key + ": {" + stringIt(value) + "},";
+            var index = attrs.ngCriteria;
+            element.removeAttr( "ng-criteria" );
+
+            scope.$watch("placeholderArray[" + index + "].selected",function(newVal,oldVal){
+                if (newVal != null) {
+                    element.next().remove();
+                    if (element.next().is("div") || element.next().is("input")) {
+                        element.next().remove();
                     }
-                }, j);
-                j = j.substring(0, j.length-1);
-                return j;
-            }
-
-            try {
-                var value = JSON.parse(attrs.pstd);
-                if (value == true || value == false) {
-                    element.html(value);
-                } else if (jQuery.type(value) === "number"){
-                    element.html(value);
-                } else {
-                    var jsonString = stringIt(value);
-                    element.html("<div'><table class=\"table table-condensed table-bordered table-hover\" style=\"margin-bottom: 0px\" >" +
-                        "<tr ng-repeat=\"(k,v) in { " + jsonString + " }\"><td style=\"font-weight: bold; color: black\">{{k}}</td>" +
-                        "<td style=\"font-weight: normal; color: darkgreen\" pstd=\"{{v}}\"></td></tr></table></div>");
-                }
-            } catch (err) {
-                var value = attrs.pstd;
-                if (jQuery.type(value) === "string") {
-                    if (value == 'PSFALSE') {
-                        element.html('false');
-                    } else if (value == 'PSTRUE') {
-                        element.html('true');
+                    if (newVal.type == 'boolean') {
+                        scope.placeholderArray[index].value='true';
+                        scope.placeholderArray[index].op='boolean';
+                        element.parent().append( "<select ng-model='placeholderArray[" + index + "].value' " +
+                            " ><option value='true' >is true</option><option value='false'>is false</option></select>" );
+                        $compile(element.next())(scope);
+                    } else if (newVal.type == 'date') {
+                        scope.placeholderArray[index].value='';
+                        scope.placeholderArray[index].op='is';
+                        element.parent().append("<select ng-model='placeholderArray[" + index + "].op' >" +
+                            "<option>is</option>" +
+                            "<option>is after</option>" +
+                            "<option>is before</option>" +
+                            "<option>is in the range</option>" +
+                            "</select>");
+                        element.parent().append("<div class='input-group' style='margin-left: 3px'>" +
+                            " <input type='text' uib-datepicker-popup='{{format}}' " +
+                            " ng-model='placeholderArray[" + index + "].value'" +
+                            " is-open='status.opened' datepicker-options='dateOptions'" +
+                            " ng-required='true' close-text='Close' />" +
+                            " <span class='input-group-btn'>" +
+                            " <button type='button' class='btn btn-sm' style='padding: 2px 5px' ng-click='open($event)'>" +
+                            " <i class='glyphicon glyphicon-calendar'></i></button>" +
+                            " </span>" +
+                            " </div>");
+                        $compile(element.next())(scope);
+                        $compile(element.next().next())(scope);
+                    } else if (newVal.type == 'integer') {
+                        scope.placeholderArray[index].value='';
+                        scope.placeholderArray[index].op='is';
+                        element.parent().append("<select ng-model='placeholderArray[" + index + "].op' >" +
+                            "<option>is</option>" +
+                            "<option>is not</option>" +
+                            "<option>is greater than</option>" +
+                            "<option>is less than</option>" +
+                            "<option>is in the range</option>" +
+                            "</select>");
+                        element.parent().append( "<input type='text' style='margin-left: 3px' " +
+                            "ng-model='placeholderArray[" + index + "].value'" +
+                            " placeholder='Enter Value' />" );
+                        $compile(element.next())(scope);
+                        $compile(element.next().next())(scope);
                     } else {
-                        element.html(value);
+                        scope.placeholderArray[index].selected = scope.fields[0];
+                        scope.placeholderArray[index].value='';
+                        scope.placeholderArray[index].op='contains';
+                        element.parent().append("<select ng-model='placeholderArray[" + index + "].op' >" +
+                            "<option>is</option>" +
+                            "<option>is not</option>" +
+                            "<option>contains</option>" +
+                            "<option>does not contain</option>" +
+                            "<option>begins with</option>" +
+                            "<option>ends with</option>" +
+                            "</select>");
+                        element.parent().append( "<input type='text' style='margin-left: 3px' " +
+                            "ng-model='placeholderArray[" + index + "].value' " +
+                            "placeholder='Enter Value' />" );
+                        $compile(element.next())(scope);
+                        $compile(element.next().next())(scope);
                     }
                 } else {
-                    console.log('unknown value type: ' + value);
+                    scope.placeholderArray[index].selected = scope.fields[0];
+                    scope.placeholderArray[index].value='';
+                    scope.placeholderArray[index].op='contains';
+                    element.parent().append("<select ng-model='placeholderArray[" + index + "].op' >" +
+                        "<option>is</option>" +
+                        "<option>is not</option>" +
+                        "<option>contains</option>" +
+                        "<option>does nto contain</option>" +
+                        "<option>begins with</option>" +
+                        "<option>ends with</option>" +
+                        "</select>");
+                    element.parent().append( "<input type='text' style='margin-left: 3px' " +
+                        "ng-model='placeholderArray[" + index + "].value' " +
+                        "placeholder='Enter Value' />" );
+                    $compile(element.next())(scope);
+                    $compile(element.next().next())(scope);
                 }
-            }
-            element.removeAttr("pstd");
-            $compile(element)(scope);
-        }
-    }
-});
-
-app.controller('nestedGridCtrl', function($scope,$rootScope,$log, uiGridColumnMenuService, myservice) {
-
-    $scope.myservice = myservice;
-    var height = $("#mygrid").innerHeight();
-    height = (height-2);
-
-    $scope.innerGridStyle = {height: height};
-
-    $scope.gridOptions = {
-        data: myservice.dataset,
-        paginationPageSizes: [10, 50, 100],
-        paginationPageSize: 10,
-        multiSelect: false,
-        enableRowSelection: true,
-        onRegisterApi: function (gridApi){
-            $scope.gridApi = gridApi;
-            gridApi.selection.on.rowSelectionChanged($scope,function(row){
-                $scope.rowEntity = row.entity;
             });
-        },
-        appScopeProvider: {
-            onDblClick : function(row) {
-
-                $scope.gridApi.selection.toggleRowSelection(row.entity);
-
-                if ($scope.gridApi.selection.getSelectedRows().length > 0) {
-                    $rootScope.$broadcast('psRowSelectEventOpen',row.entity);
-                } else {
-                    $rootScope.$broadcast('psRowSelectEventClose');
-                }
-            }
-        },
-
-        rowTemplate: "<div ng-dblclick=\"grid.appScope.onDblClick(row)\" style=\"cursor: pointer;\" " +
-        "ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" " +
-        "class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell ></div>"
-
+        }
     };
-
-});
-
-app.service('myservice', function() {
-});
-
-app.controller('quickviewCtrl', function($scope, $rootScope, myservice) {
-
-    $scope.myservice = myservice;
-    var height = $("#mygrid").innerHeight();
-    height = (height-2);
-    $scope.mystyle = { 'max-height': '500px', width: $scope.myservice.width, overflow: 'auto'};
-    $scope.gridOptions = {
-        data: [$scope.myservice.data]
-    };
-
-    $scope.$watch('myservice.width', function(newValue, oldValue) {
-        $scope.mystyle = { 'max-height': '500px', width: newValue, overflow: 'auto'};
-    });
-
-    $scope.$watch('myservice.data', function(newValue, oldValue) {
-        $scope.gridOptions.data.splice(0,1);
-        $scope.gridOptions.data.push(newValue);
-    });
-
-});
-
-app.controller('mainCtrl', function($scope,uiGridConstants,myservice) {
-
-    $scope.$on('psRowSelectEventOpen', function(event,data){
-        $scope.gridOptions.columnDefs[0].visible = true;
-        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.COLUMN);
-
-        if ($scope.gridApi.saveState.save().columns[0].width == '25%') {
-            var gridWidth = $("#mygrid").innerWidth();
-            var calculatedLeftWidth = (gridWidth * .25);
-            myservice.width = calculatedLeftWidth;
-        }
-
-        myservice.data = data;
-    });
-
-    $scope.$on('psRowSelectEventClose', function(event) {
-        $scope.gridOptions.columnDefs[0].visible = false;
-        $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.COLUMN);
-    });
-
-    $scope.myservice = myservice;
-
-    myservice.dataset = [];
-    for (var x=0;x<1000;x++) {
-        myservice.dataset.push(
-            {
-                a: guid(),b: guid(),
-                c: { c_1: guid(), c_2: guid()},
-                d: { d_1: [guid(),guid(),guid()]},
-                e: { e_1: true, e_2: 100, e_3: guid()},
-                f: { f_1: [{ f_1_a: guid()},{ f_1_b: guid()},{ f_1_c: guid()}]}
-            }
-        );
-    }
-
-    function guid() {
-        function _p8(s) {
-            var p = (Math.random().toString(16)+"000000000").substr(2,8);
-            return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
-        }
-        return _p8() + _p8(true) + _p8(true) + _p8();
-    }
-
-    $scope.gridOptions = {
-        data: [],
-        columnDefs: [
-            { field: 'quickview', displayName: 'LHS', width: '25%', visible: false, headerCellTemplate: 'quickviewHeader.html' },
-            { field: 'nestedgrid', displayName: 'RHS', headerCellTemplate: 'nestedgridHeader.html'}
-        ],
-        enableColumnMenus: false,
-        onRegisterApi: function (gridApi){
-            $scope.gridApi = gridApi;
-
-            gridApi.colResizable.on.columnSizeChanged($scope,function(colDef, deltaChange){
-                var s = $scope.gridApi.saveState.save();
-                var leftWidth = s.columns[0].width
-                myservice.width = leftWidth;
-            });
-
-        }
-    }
-
 });
